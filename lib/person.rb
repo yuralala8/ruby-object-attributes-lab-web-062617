1471b7e428fd61b71f240cea77963e9baaa8737c
yuralala8 <yc1435@nyu.edu>
2017-06-28 11:39:01 -0400

    object attributes

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def name
+        @name
+    end
+    
+    def name=(persons_name)
+        @name = persons_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(dogs_job)
+        @job = dogs_job
+    end
+
+end
